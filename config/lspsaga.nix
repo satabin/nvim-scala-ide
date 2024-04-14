7a434b2864c4723b43cfe0fe71bbc75df891f35b
Lucas Satabin <lucas.satabin@gnieh.org>
2024-04-14 10:26:15 +0200

    Add floating terminal toggling

--- config/lspsaga.nix
+++ config/lspsaga.nix
@@ -3,4 +3,20 @@
     enable = true;
     lightbulb.enable = false;
   };
+
+  userCommands = {
+    Terminal.command = "Lspsaga term_toggle";
+  };
+
+  keymaps = [
+    {
+      mode = [ "n" "t" ];
+      key = "<space>t";
+      action = "<cmd>Lspsaga term_toggle<CR>";
+      options = {
+        noremap = true;
+        silent = true;
+      };
+    }
+  ];
 }
