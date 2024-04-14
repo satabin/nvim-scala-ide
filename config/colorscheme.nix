8cab0e102a858c14f298c141bc79af719f776ae6
Lucas Satabin <lucas.satabin@gnieh.org>
2024-04-14 10:07:47 +0200

    Use override feature for lense highlighting

--- config/colorscheme.nix
+++ config/colorscheme.nix
@@ -1,11 +1,7 @@
 {
   colorschemes.vscode.enable = true;
-  extraConfigLua = /* lua */ ''
-    local links = {
-      ['LspCodeLens'] = '@comment',
-    }
-    for newgroup, oldgroup in pairs(links) do
-      vim.api.nvim_set_hl(0, newgroup, { link = oldgroup, default = true })
-    end
-  '';
+
+  highlightOverride = {
+    LspCodeLens.link = "@comment";
+  };
 }
