0bde4e1241bf449f044ca2f3161f14c2a582a4b3
Lucas Satabin <lucas.satabin@gnieh.org>
2024-04-14 12:09:33 +0200

    Add flog support for viewing git graph

--- config/git.nix
+++ config/git.nix
@@ -1,4 +1,9 @@
-{
+{ pkgs, ... }: {
   plugins.neogit.enable = true;
   plugins.gitsigns.enable = true;
+  plugins.fugitive.enable = true;
+
+  extraPlugins = [
+    pkgs.vimPlugins.vim-flog
+  ];
 }
